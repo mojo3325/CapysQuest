59f632700a93cbfc909b178e84987358e8339fb7
mojo3325 <ustenkoegorp100dl@gmail.com>
2023-04-14 17:32:56 +0300

    WelcomeLabel fix

--- Assets/Scripts/Ui/Screens/HomeScreen.cs
+++ Assets/Scripts/Ui/Screens/HomeScreen.cs
@@ -1,11 +1,34 @@
-using UnityEngine;
+using UnityEngine.UIElements;
 
 public class HomeScreen : MenuScreen
 {
+
+    private Label _welcomeLabel;
+    
+    private static string _welcomeLabelName = "WelcomeLabel";
+    private Tools _tools = new();
+
     protected override void SetVisualElements()
     {
         base.SetVisualElements();
+
+        _welcomeLabel = _root.Q<Label>(_welcomeLabelName);
         _showMenuBar = true;
+        SetupSizes();
+    }
+    
+    private void SetupSizes()
+    {
+        var devicetype = _tools.GetDeviceType();
+
+        if (devicetype == DeviceType.Phone)
+        {
+            _welcomeLabel.style.fontSize = new StyleLength(220);
+        }
+        else if(devicetype == DeviceType.Tablet)
+        {
+            _welcomeLabel.style.fontSize = new StyleLength(180);
+        }
     }
 
     private void OnEnable()
